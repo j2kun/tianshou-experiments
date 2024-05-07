208e55a43bd094cda4f0c9a02f6f66c6b5fcb45e
Jeremy Kun <j2kun@users.noreply.github.com>
2024-05-06 17:26:22 -0700

    try adding custom environment

---
+++ custom_env.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+from gymnasium import spaces
+from gymnasium.envs.registration import register
+import gymnasium as gym
+
+
+class GridWorldEnv(gym.Env):
+    metadata = {"render_modes": [], "render_fps": 4}
+
+    def __init__(self, render_mode=None, size=5):
+        self.size = size  # The size of the square grid
+        self.window_size = 512  # The size of the PyGame window
+
+        # Observations are dictionaries with the agent's and the target's location.
+        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
+        self.observation_space = spaces.Dict(
+            {
+                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = spaces.Discrete(4)
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        self._action_to_direction = {
+            0: np.array([1, 0]),
+            1: np.array([0, 1]),
+            2: np.array([-1, 0]),
+            3: np.array([0, -1]),
+        }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+    def _get_obs(self):
+        return {"agent": self._agent_location, "target": self._target_location}
+
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(
+                self._agent_location - self._target_location, ord=1
+            )
+        }
+
+    def reset(self, seed=None, options=None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+
+        # Choose the agent's location uniformly at random
+        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+
+        # We will sample the target's location randomly until it does not coincide with the agent's location
+        self._target_location = self._agent_location
+        while np.array_equal(self._target_location, self._agent_location):
+            self._target_location = self.np_random.integers(
+                0, self.size, size=2, dtype=int
+            )
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, info
+
+    def step(self, action):
+        # Map the action (element of {0,1,2,3}) to the direction we walk in
+        direction = self._action_to_direction[action]
+        # We use `np.clip` to make sure we don't leave the grid
+        self._agent_location = np.clip(
+            self._agent_location + direction, 0, self.size - 1
+        )
+        # An episode is done iff the agent has reached the target
+        terminated = np.array_equal(self._agent_location, self._target_location)
+        reward = 1 if terminated else -0.1  # Binary sparse rewards
+        observation = self._get_obs()
+        info = self._get_info()
+
+        truncated = False
+        return observation, reward, terminated, truncated, info
+
+    def render(self):
+        pass
+
+    def close(self):
+        pass
+
+
+register(
+     id="gym_examples/GridWorld-v0",
+     entry_point="custom_env:GridWorldEnv",
+     max_episode_steps=300,
+     reward_threshold=1.0,
+)
+
+env = gym.make('gym_examples/GridWorld-v0')
--- dqn.py
+++ dqn.py
@@ -1,4 +1,83 @@
+from torch import nn
 import gymnasium as gym
+import numpy as np
 import tianshou as ts
+import torch
+import custom_env
+from gymnasium.wrappers import FlattenObservation
 
-env = gym.make('CartPole-v1')
+num_training_envs = 10
+num_test_envs = 100
+env_id = "gym_examples/GridWorld-v0"
+
+
+def make_env():
+    return FlattenObservation(gym.make(env_id))
+
+
+env = make_env()
+
+train_envs = ts.env.ShmemVectorEnv([make_env for _ in range(num_training_envs)])
+test_envs = ts.env.ShmemVectorEnv([make_env for _ in range(num_test_envs)])
+
+
+class Net(nn.Module):
+    def __init__(self, state_shape, action_shape):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(np.prod(state_shape), 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, np.prod(action_shape)),
+        )
+
+    def forward(self, obs, state=None, info={}):
+        if not isinstance(obs, torch.Tensor):
+            obs = torch.tensor(obs, dtype=torch.float)
+        batch = obs.shape[0]
+        logits = self.model(obs.view(batch, -1))
+        return logits, state
+
+
+state_shape = env.observation_space.shape or env.observation_space.n
+action_shape = env.action_space.shape or env.action_space.n
+net = Net(state_shape, action_shape)
+optim = torch.optim.Adam(net.parameters(), lr=1e-3)
+
+policy = ts.policy.DQNPolicy(
+    model=net,
+    optim=optim,
+    action_space=env.action_space,
+    discount_factor=0.9,
+    estimation_step=3,
+    target_update_freq=320,
+)
+
+train_collector = ts.data.Collector(
+    policy,
+    train_envs,
+    ts.data.VectorReplayBuffer(20000, num_training_envs),
+    exploration_noise=True,
+)
+test_collector = ts.data.Collector(policy, test_envs, exploration_noise=True)
+
+result = ts.trainer.OffpolicyTrainer(
+    policy=policy,
+    train_collector=train_collector,
+    test_collector=test_collector,
+    max_epoch=10,
+    step_per_epoch=1000,
+    step_per_collect=10,
+    update_per_step=0.1,
+    episode_per_test=100,
+    batch_size=64,
+    train_fn=lambda epoch, env_step: policy.set_eps(0.1),
+    test_fn=lambda epoch, env_step: policy.set_eps(0.05),
+    stop_fn=lambda mean_rewards: mean_rewards >= env.spec.reward_threshold,
+).run()
+print(f'Finished training! Use {result["duration"]}')
+
+torch.save(policy.state_dict(), "dqn.pth")
